0092618d11f51b9d075e703ffaa501d9f24bd7d2
abdulwahed-dev <abdulwahed.dev@gmail.com>
2022-10-18 15:37:49 +0300

    Creating The Sign Up Page

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home
+from .views import RegisterView, home  # Import the view here
 
 urlpatterns = [
     path('', home, name='users-home'),
+    path('register/', RegisterView.as_view(), name='users-register'),  # This is what we added
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.views import View
+
+from .forms import RegisterForm
 
 
 def home(request):
     return render(request, 'users/home.html')
+
+class RegisterView(View):
+    form_class = RegisterForm
+    initial = {'key': 'value'}
+    template_name = 'users/register.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+
+            return redirect(to='/')
+
+        return render(request, self.template_name, {'form': form})
